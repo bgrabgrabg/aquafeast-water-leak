dcd2bd2b78a04943ab04f53b7a53b3dddd0d9d3c
bgrabgrabg <82815639+bgrabgrabg@users.noreply.github.com>
2026-07-07 10:04:41 +0300

    Create binary_sensor.py

---
+++ custom_components/aquafeast_water_leak/binary_sensor.py
@@ -0,0 +1,14 @@
+"""Binary sensor platform for Aquafeast Water Leak."""
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up Aquafeast binary sensors."""
+    async_add_entities([])

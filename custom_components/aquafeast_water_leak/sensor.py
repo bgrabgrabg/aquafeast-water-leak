8fff2ba365bafe8c10e9f9aaae30c69c6fc00620
bgrabgrabg <82815639+bgrabgrabg@users.noreply.github.com>
2026-07-07 10:04:18 +0300

    Create sensor.py

---
+++ custom_components/aquafeast_water_leak/sensor.py
@@ -0,0 +1,14 @@
+"""Sensor platform for Aquafeast Water Leak."""
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up Aquafeast sensors."""
+    async_add_entities([])

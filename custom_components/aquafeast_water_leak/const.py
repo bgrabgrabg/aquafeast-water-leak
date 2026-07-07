961f44b2ef5d3017fba7194645372d22e40f0a2d
bgrabgrabg <82815639+bgrabgrabg@users.noreply.github.com>
2026-07-07 10:01:51 +0300

    Create const.py

---
+++ custom_components/aquafeast_water_leak/const.py
@@ -0,0 +1,11 @@
+"""Constants for Aquafeast Water Leak."""
+
+DOMAIN = "aquafeast_water_leak"
+NAME = "Aquafeast Water Leak"
+VERSION = "0.0.1"
+
+CONF_MAC = "mac_address"
+CONF_DEVICE_MODEL = "device_model"
+
+DEFAULT_DEVICE_MODEL = "BSK_BR"
+DEFAULT_SCAN_INTERVAL = 30
